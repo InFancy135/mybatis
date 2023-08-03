bb36db7ef9e9408ee515d9a19f2ba81815cbaa47
InFancy13 <gjwogns1010@naver.com>
2023-08-03 11:27:14 +0900

    건웅시치

--- src/main/java/dao/MemberDaoMybatis.java
+++ src/main/java/dao/MemberDaoMybatis.java
@@ -2,10 +2,17 @@ package dao;
 
 import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+
+import config.MybatisConfig;
 import vo.Member;
 
 public class MemberDaoMybatis implements MemberDao {
 
+	private final 
+	SqlSessionFactory sqlSessionFactory = MybatisConfig.getSqlSessionFactory();
+	
 	@Override
 	public Member saveMember(Member member) {
 		return null;
@@ -13,9 +20,23 @@ public class MemberDaoMybatis implements MemberDao {
 
 	@Override
 	public Member findMemberById(Long id) {
-		return null;
+	      //SqlSessionFactory 에 SqlSession 객체를 얻는다
+		SqlSession sqlSession = sqlSessionFactory.openSession();
+		Member member = null;
+		try {
+	      
+	      //SqlSession과 Mapper 인터페이스를 연결한다 
+			MemberMapper mapper = sqlSession.getMapper(MemberMapper.class);
+			member = mapper.findMemberById(id);
+	      
+		}catch(Exception e) {
+	         e.printStackTrace();
+		}finally {
+			if(sqlSession != null) sqlSession.close();
+		}
+		return member;
 	}
-
+	
 	@Override
 	public List<Member> findMembers() {
 		return null;
@@ -30,4 +51,9 @@ public class MemberDaoMybatis implements MemberDao {
 	public void removeMember(Long id) {
 	}
 
+	public static void main(String[] args) {
+		MemberDao dao = new MemberDaoMybatis();
+		Member member = dao.findMemberById(1L);
+		System.out.println("member: " + member);
+	}
 }
